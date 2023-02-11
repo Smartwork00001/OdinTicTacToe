38eb74cabd4a0e1b029f84b8d8bcdc428cf8aded
Teibok Chyne <smartwork00001@gmail.com>
2023-02-11 13:34:28 +0530

    Add faulty game winning logic

--- scripts.js
+++ scripts.js
@@ -1,18 +1,43 @@
 const boardDiv = document.querySelector("#board");
 const mO = document.querySelector("#mO");
 const mX = document.querySelector("#mX");
+const okButton = document.querySelector("#okButton");
+const okButtonDiv = document.querySelector(".okButtonDiv");
+const okButtonText = document.querySelector("#okButtonText");
+const newGame = document.querySelector("#new-game");
+
+function removeMessage() {
+  okButtonDiv.style.display = "none";
+}
+
+function displayMessage(winner) {
+  okButtonText.textContent = `Player ${winner} has won`;
+  okButtonDiv.style.display = "flex";
+}
 
 const gameBoard = (function (doc) {
   const board = [];
   let move;
+  let turn = 1;
+  let player1;
+  let player2;
+
+  function checkEqual(a, b, c) {
+    if (board[a] !== " " && board[a] === board[b] && board[b] === board[c]) {
+      return board[a];
+    }
+    return false;
+  }
 
   function setInitialMove(m) {
     move = m;
+    player1 = m;
+    player2 = m === "O" ? "X" : "O";
   }
 
   function initBoard() {
     for (let i = 1; i < 10; i += 1) {
-      board[i] = i;
+      board[i] = " ";
     }
   }
 
@@ -37,15 +62,66 @@ const gameBoard = (function (doc) {
     }
   }
 
-  return { initBoard, displayBoard, updateBoard, setInitialMove };
+  function hasWon() {
+    let won = checkEqual(1, 2, 3);
+    if (won !== false) {
+      displayMessage(won === player1 ? "1" : "2");
+      return true;
+    }
+    won = checkEqual(4, 5, 6);
+    if (won !== false) {
+      displayMessage(won === player1 ? "1" : "2");
+      return true;
+    }
+    won = checkEqual(7, 8, 9);
+    if (won !== false) {
+      displayMessage(won === player1 ? "1" : "2");
+      return true;
+    }
+    won = checkEqual(1, 4, 7);
+    if (won !== false) {
+      displayMessage(won === player1 ? "1" : "2");
+      return true;
+    }
+    won = checkEqual(2, 5, 8);
+    if (won !== false) {
+      displayMessage(won === player1 ? "1" : "2");
+      return true;
+    }
+    won = checkEqual(3, 6, 9);
+    if (won !== false) {
+      displayMessage(won === player1 ? "1" : "2");
+      return true;
+    }
+    won = checkEqual(1, 5, 9);
+    if (won !== false) {
+      displayMessage(won === player1 ? "1" : "2");
+      return true;
+    }
+    won = checkEqual(3, 5, 7);
+    if (won !== false) {
+      displayMessage(won === player1 ? "1" : "2");
+      return true;
+    }
+    return false;
+  }
+
+  return { initBoard, displayBoard, updateBoard, setInitialMove, hasWon };
 })(document);
 
-gameBoard.displayBoard();
+function createNewGame() {
+  gameBoard.setInitialMove("");
+  gameBoard.initBoard();
+  gameBoard.displayBoard();
+}
 
 function handleBoardEvent(event) {
   const child = event.target.closest(".board-key");
   if (child) {
     gameBoard.updateBoard(child.getAttribute("id"));
+    if (gameBoard.hasWon()) {
+      createNewGame();
+    }
   }
 }
 
@@ -65,3 +141,7 @@ function moveHandler(event) {
 boardDiv.addEventListener("click", handleBoardEvent);
 mO.addEventListener("click", moveHandler);
 mX.addEventListener("click", moveHandler);
+newGame.addEventListener("click", createNewGame);
+okButton.addEventListener("click", removeMessage);
+
+createNewGame();
